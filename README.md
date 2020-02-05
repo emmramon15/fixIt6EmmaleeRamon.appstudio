cb0ceaa2a953efda12a536cc887238a17b0d43e2
emmramon15 <60593785+emmramon15@users.noreply.github.com>
2020-02-05 14:53:27 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fixit6EmmaleeRamon.appstudio
+ fixit6
---
+++ forms/fixit6GH/fixit6GH.js
@@ -0,0 +1,8 @@
+let name = prompt("Enter your best friend: ")
+let city = prompt("Enter which city your best friend is from: ")
+
+if (name == "Emmalee" && city == "Brookfield")
+  alert(`Your bestfriend is ${name} and is from ${city}.`)
+else
+  alert("That is not your bestfriend")
+
